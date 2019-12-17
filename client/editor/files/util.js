73487a35434328a5349cb289452f29f96ff81ef1
Joseph <joseph42@live.co.uk>
2019-12-17 04:38:09 +0000

    Cleanup

--- client/editor/files/util.js
+++
@@ -1,7 +0,0 @@
-function DocReady(fn) {
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-    setTimeout(fn, 1);
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
-} 
